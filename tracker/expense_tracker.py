074a733c058e1321db28a59ab8ba251d95f5c8c0
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2024-02-12 19:38:21 -0300

    Add total expenses (#91)

--- tracker/expense_tracker.py
+++ tracker/expense_tracker.py
@@ -28,7 +28,12 @@ class ExpenseTracker:
         )
         return [row[0] for row in categories_matrix]  # Extract column from matrix
 
-    def last_expenses(self, number: int):
+    def last_expenses(self, number: int = 0) -> list[Expense]:
+        '''
+        Gets the last n expenses.
+
+        number -- the number of expenses to obtain.
+        '''
         current_date = date.today()
         worksheet_title = self.editor.get_worksheet_name(current_date)
         worksheet = self.editor.open_worksheet(worksheet_title)
@@ -37,6 +42,21 @@ class ExpenseTracker:
         filtered = filter(lambda expense: len(expense) > 3, expenses_matrix)
 
         expenses = map(lambda row: Expense(spent_at=row[0], description=row[2], location=row[3],
-                                           price=row[4], category=row[5]).to_markdown(), filtered)
+                                           price=row[4], category=row[5]), filtered)
 
         return list(expenses)[-number:]
+
+    def last_expenses_as_markdown(self, number: int = 0) -> list[str]:
+        '''
+        Gets the last n expenses in markdown format.
+
+        number -- the number of expenses to obtain.
+        '''
+        return [expense.to_markdown() for expense in self.last_expenses(number)]
+
+    def total_expenses(self) -> int:
+        '''
+        Gets the sum of expense for the current month.
+        '''
+        expenses = self.last_expenses()
+        return sum(int(expense.price) for expense in expenses)
