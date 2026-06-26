ddd5d90f1256ebf18a4c2d57664b5ddba5b5a0c6
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2026-06-26 01:09:21 -0300

    Migrate to uv (#126)
    * Add uv
    
    * Updgrade python version
    
    * Upgrade to latest dependencies
    
    * Update build GHA
    
    * Fix GHA
    
    * Update linter GHA
    
    * Set linter ignore path
    
    * Fix linter exclusion
    
    * update .flake8
    
    * Fix linter error
    
    * Update Dockerfile
    
    * Remove Procfile
    
    * Remove name and url from Dockerfile
    
    * Fix uv repo url

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: python3 -m tracker.bot
-worker: python3 -m tracker.bot
--- prepush.sh
+++ prepush.sh
@@ -1,15 +1,15 @@
 # Flake 8
 echo 'flake8'
-pipenv run flake8 --config=.flake8
+uv run flake8 --config=.flake8
 # Pylint
 echo '-------------------'
 echo 'pylint'
-pipenv run pylint */*.py --disable=missing-docstring --errors-only
+uv run pylint */*.py --disable=missing-docstring --errors-only
 # MyPy
 echo '-------------------'
 echo 'mypy'
-pipenv run mypy tracker
+uv run mypy tracker
 # Tests
 echo '-------------------'
 echo 'tests'
-pipenv run pytest -c pytest.ini --cov --cov-config .coveragerc
+uv run pytest -c pytest.ini --cov --cov-config .coveragerc
